f6f560910254b85692b898fad34a6d25d09686ec
jeffhappily <jeff@ridebeam.com>
2021-05-03 09:19:49 +0800

    Clean up

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# malodivo
+# Malodivo
 
 ## Execute  
 
--- src/Types.hs
+++ src/Types.hs
@@ -1,71 +1,73 @@
 {-# LANGUAGE DuplicateRecordFields #-}
-{-# LANGUAGE TemplateHaskell       #-}
+{-# LANGUAGE GeneralisedNewtypeDeriving #-}
+{-# LANGUAGE TemplateHaskell #-}
+
 module Types where
 
-import           Control.Lens  (classUnderscoreNoPrefixFields, makeLensesWith)
-import           Data.Aeson
-import           Data.Aeson.TH
-import           Data.Ratio    ((%))
-import           RIO
-import qualified RIO.Map       as M
-import           RIO.Process   (HasProcessContext (..), ProcessContext)
+import Control.Lens (classUnderscoreNoPrefixFields, makeLensesWith)
+import Data.Aeson
+import Data.Aeson.TH
+import Data.Ratio ((%))
+import RIO
+import qualified RIO.Map as M
+import RIO.Process (HasProcessContext (..), ProcessContext)
 
 data App = App
-  { appLogFunc        :: !LogFunc
-  , appProcessContext :: !ProcessContext
+  { appLogFunc :: !LogFunc,
+    appProcessContext :: !ProcessContext
   }
 
 instance HasLogFunc App where
-  logFuncL = lens appLogFunc (\x y -> x { appLogFunc = y })
+  logFuncL = lens appLogFunc (\x y -> x {appLogFunc = y})
+
 instance HasProcessContext App where
-  processContextL = lens appProcessContext (\x y -> x { appProcessContext = y })
+  processContextL = lens appProcessContext (\x y -> x {appProcessContext = y})
 
 type CategoryName = String
+
 type BillName = String
+
 type Amount = Integer
 
-data Input =
-  Input
-    { bills     :: [Bill]
-    , districts :: [District]
-    }
-    deriving Show
+data Input = Input
+  { bills :: [Bill],
+    districts :: [District]
+  }
+  deriving (Show)
 
-data Bill =
-  Bill
-    { _name     :: BillName
-    , _category :: CategoryName
-    , _amount   :: Int
-    }
-    deriving Show
+data Bill = Bill
+  { _name :: BillName,
+    _category :: CategoryName,
+    _amount :: Int
+  }
+  deriving (Show)
 
-data District =
-  District
-    { _name                   :: String
-    , _availableFunds         :: Int
-    , _categoryDefaultFunding :: Map CategoryName Amount
-    , _billSpecificFunding    :: Map BillName Amount
-    , _caps                   :: Map CategoryName Amount
-    }
-    deriving Show
+data District = District
+  { _name :: String,
+    _availableFunds :: Int,
+    _categoryDefaultFunding :: Map CategoryName Amount,
+    _billSpecificFunding :: Map BillName Amount,
+    _caps :: Map CategoryName Amount
+  }
+  deriving (Show)
 
 instance FromJSON District where
   parseJSON = withObject "District" $ \v -> do
-    _name                   <- v .: "name"
-    _availableFunds         <- v .: "availableFunds"
-    categoryDefaultFunding  <- v .: "categoryDefaultFunding"
-    billSpecificFunding     <- v .: "billSpecificFunding"
-    caps                    <- v .: "caps"
+    _name <- v .: "name"
+    _availableFunds <- v .: "availableFunds"
+    categoryDefaultFunding <- v .: "categoryDefaultFunding"
+    billSpecificFunding <- v .: "billSpecificFunding"
+    caps <- v .: "caps"
 
     let _categoryDefaultFunding = categoryAmountToMap categoryDefaultFunding
     let _billSpecificFunding = billAmountToMap billSpecificFunding
     let _caps = categoryAmountToMap caps
 
-    return District{..}
+    return District {..}
 
 data CategoryAmount = CategoryAmount
-  { _category :: CategoryName
-  , _amount   :: Amount
+  { _category :: CategoryName,
+    _amount :: Amount
   }
   deriving (Show)
 
@@ -73,8 +75,8 @@ categoryAmountToMap :: [CategoryAmount] -> Map CategoryName Amount
 categoryAmountToMap = foldr (\(CategoryAmount c a) z -> M.insert c a z) M.empty
 
 data BillAmount = BillAmount
-  { _bill   :: BillName
-  , _amount :: Amount
+  { _bill :: BillName,
+    _amount :: Amount
   }
   deriving (Show)
 
@@ -85,38 +87,21 @@ makeLensesWith classUnderscoreNoPrefixFields ''Bill
 makeLensesWith classUnderscoreNoPrefixFields ''District
 
 $(deriveJSON defaultOptions 'Input)
-$(deriveJSON defaultOptions { fieldLabelModifier = drop 1 } 'Bill)
-$(deriveToJSON defaultOptions { fieldLabelModifier = drop 1 } 'District)
-$(deriveJSON defaultOptions { fieldLabelModifier = drop 1 } 'CategoryAmount)
-$(deriveJSON defaultOptions { fieldLabelModifier = drop 1 } 'BillAmount)
+$(deriveJSON defaultOptions {fieldLabelModifier = drop 1} 'Bill)
+$(deriveToJSON defaultOptions {fieldLabelModifier = drop 1} 'District)
+$(deriveJSON defaultOptions {fieldLabelModifier = drop 1} 'CategoryAmount)
+$(deriveJSON defaultOptions {fieldLabelModifier = drop 1} 'BillAmount)
 
 -- | Constraint is a function that given a value, returns a
 -- constrained value
-newtype Constraint' a b =
-  Constraint
-    { runConstraint
-        :: a -- ^ Initial value
-        -> b -- ^ New value
-    }
-    deriving Category
+newtype Constraint' a b = Constraint
+  { runConstraint ::
+      a -> -- Initial value
+      b -- New value
+  }
+  deriving (Category, Functor, Applicative, Monad)
 
 type Constraint a = Constraint' a a
-
-instance Functor (Constraint' a) where
-  fmap f (Constraint g) = Constraint $ f . g
-
-instance Applicative (Constraint' a) where
-  pure = Constraint . const
-
-  Constraint f <*> Constraint g = Constraint $ \x ->
-    f x (g x)
-
-instance Monad (Constraint' a) where
-  return = pure
-
-  Constraint g >>= f = Constraint $ \x' ->
-    let Constraint h = f (g x')
-    in h x'
 
 -- | Bill specific fund
 bsfConstraint :: Maybe Integer -> Constraint Integer
@@ -129,10 +114,9 @@ capConstraint num = Constraint $ \x -> min x num
 -- |
 ratioConstraint :: Integer -> Constraint [Integer]
 ratioConstraint x = Constraint $ \xs ->
-  let
-    total = sum xs
-    ratio = x % total
-    convertRatio n = round (fromRational $ fromIntegral n * ratio :: Double) in
-  if total > x
-    then fmap convertRatio xs
-    else xs
+  let total = sum xs
+      ratio = x % total
+      convertRatio n = round (fromRational $ fromIntegral n * ratio :: Double)
+   in if total > x
+        then fmap convertRatio xs
+        else xs
